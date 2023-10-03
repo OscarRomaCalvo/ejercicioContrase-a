6cfdfc1a972aaa5d61450f17732c0ddf1fba915f
OscarRomaCalvo <oscar.roma.calvo@alumnos.upm.es>
2023-10-03 11:46:53 +0200

    Ejercicio a medio hacer

---
+++ ejercicioContrasena/src/components/FormularioContrasena/FormularioContrasena.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react"
+
+function FormularioContrasena(){
+    const [password, setPassword] = useState("")
+
+    function handleOnChangePassword(event){
+        const introducedPassword = event.target.value
+        setPassword(introducedPassword)
+    }
+
+    function checkPassword(){
+        let puntuacion = 0
+        puntuacion += checkLongitud()
+        puntuacion += checkCaracteres()
+
+        window.alert(puntuacion)
+        event.preventDefault()
+    }
+
+    function checkLongitud(){
+        let longitud = password.length
+        if(longitud<=6)
+            return 0
+        else if(longitud<=8)
+            return 1
+        else if(longitud<=12)
+            return 2
+        else
+            return 3
+    }
+    
+    function checkLetras(){
+        const mayusculas = /A-Z]/
+        const minusculas = /[a-z]/
+
+        if(mayusculas.test(password) && minusculas.test(password))
+            return 2
+        else if(mayusculas.test(password) || minusculas.test(password))
+            return 1
+        else 
+            return 0
+    }
+
+    function checkNumeros(){
+        return 0
+    }
+
+    function checkEspeciales(){
+        return 0
+    }
+
+    function checkCaracteres(){
+        return checkLetras()+checkNumeros()+checkEspeciales()
+    
+    }
+    return(
+        <form onSubmit={checkPassword}>
+            <p>Contraseña</p>
+            <input type="text" name="Password" value={password} onChange={handleOnChangePassword}></input>
+            <input type="submit" value="Enviar"></input>
+        </form>
+    )
+}
+
+export default FormularioContrasena
